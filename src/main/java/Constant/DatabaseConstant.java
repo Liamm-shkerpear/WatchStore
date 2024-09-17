a5e92e4d0c1bae1dff4bab165f72a9d1a9b9a17f
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2024-09-17 20:30:33 +0700

    Create DatabaseConstant.java
    DB constant

---
+++ src/main/java/Constant/DatabaseConstant.java
@@ -0,0 +1,8 @@
+package Constant;
+
+public interface DatabaseConstant {
+    public static String DRIVER_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String URL = "jdbc:sqlserver://localhost:1433;databaseName=SWP_Projects;encrypt=true;TrustServerCertificate=true";
+    public static String USER_NAME = "sa";
+    public static String PASS_WORD = "123123";
+}
